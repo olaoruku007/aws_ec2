729047506737fc7beb56e31d6be41541e1624c1d
Ola Oruku <olaoruku007@gmail.com>
2023-09-11 15:54:48 -0400

    Include terraform configuration files

---
+++ main.tf
@@ -0,0 +1,59 @@
+data "aws_key_pair" "Dev_KP_OH" {
+  key_name   = "Dev_KP_OH"
+
+}
+
+
+resource "aws_instance" "class-webserver" {
+  ami           = var.ami
+  instance_type = var.instance_type
+  key_name   = "Dev_KP_OH"
+  vpc_security_group_ids = [aws_security_group.SG.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo dnf update -y
+              sudo dnf install httpd -y
+              sudo systemctl enable httpd
+              sudo systemctl start httpd
+              EOF
+
+  tags = {
+    Name = "terraform-class-webserver"
+  }
+  
+}
+
+resource "aws_security_group" "SG" {
+  name = "Terraform-webserver-SG"
+
+  ingress {
+    protocol  = "tcp"
+    from_port = var.server_port
+    to_port   = var.server_port
+    cidr_blocks = ["0.0.0.0/0"]
+
+  
+  }
+
+
+  ingress {
+    protocol  = "tcp"
+    from_port = var.server_port2
+    to_port   = var.server_port2
+    cidr_blocks = ["0.0.0.0/0"]
+
+  
+  }
+
+
+
+  egress {
+    from_port   = var.server_port1
+    to_port     = var.server_port1
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+
+   
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,35 @@
+variable "region" {
+  description = "The region the resources will be launched"
+  type        = string
+  default     = "us-east-2"
+}
+
+variable "ami" {
+  description = "The instance ami for the webserver"
+  type        = string
+  default     = "ami-0a04068a95e6a1cde"
+}
+
+variable "instance_type" {
+  description = "The instance type for the instance"
+  type        = string
+  default     = "t2.small"
+}
+
+variable "server_port" {
+  description = "The server port for the instance"
+  type        = number
+  default     = 22
+}
+
+variable "server_port1" {
+  description = "The server port for the instance"
+  type        = number
+  default     = 0
+}
+
+variable "server_port2" {
+  description = "The server port for the instance"
+  type        = number
+  default     = 80
+}
